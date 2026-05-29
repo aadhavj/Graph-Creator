533a4491a2fb9b6fea731353887f178d2fc50e02
U-BSD\483830 <483830@SHS-FYBF6X3.beaverton.k12.or.us>
2026-05-29 09:46:13 -0700

    did part of algorithm

--- main.cpp
+++ main.cpp
@@ -27,6 +27,31 @@ struct graph{
 		edges.push_back(new edge(a,b,w));
 		a->paths.push_back(b);
 	}
+	void deleteEdge(vertex* a, vertex* b) {
+		if (a && b){
+			int index = getIndex(a->paths, b);
+			if (index != -1){
+				a->paths.erase(a->paths.begin()+index);
+				for (int i = 0;i< edges.size();i++){
+					if ((edges[i]->v1 == a) && (edges[i]->v2 == b)){
+						edge* deleteMe = edges[i];
+						edges.erase(edges.begin()+i);
+						delete deleteMe;
+						break;
+					}
+				}
+			}
+		}
+	}
+	void deleteVertex(vertex* a){
+		if (a){
+			for (vertex* pathway : a->paths){
+				deleteEdge(a,pathway);
+			}
+			vertices.erase(vertices.begin()+getIndex(vertices,a));
+			delete a;
+		}
+	}
 	vertex* locate(string name) {for (const auto& v : vertices) {if (v->name == name){return v;}}return nullptr;}
 	int getIndex(vector<vertex*> a, vertex* b){for (int i = 0;i < a.size(); i++){if (a[i] == b){return i;}}return -1;}
 	void printGraph() {
@@ -43,14 +68,51 @@ struct graph{
 	}
 	int getEdgeWeight(vertex* v1, vertex* v2){for (const auto& e : edges) {if (e->v1 == v1 && e->v2 == v2) {return e->weight;}}}
 	int findSmallestDist(vertex* v1, vertex* v2){
+		
 		vector<vertex*> visited;
 		vector<vertex*> unvisited = vertices;
 		vector<int> shortestLengths(vertices.size(), 99999);
 		vector<vertex*> prevVertex(vertices.size(),nullptr);
 		shortestLengths[find(vertices.begin(), vertices.end(), v1)-vertices.begin()] = 0;
+
+		vertex* current;
+		int currIndex;
+
+		while (unvisited.size() > 0){
+
+			//Gets minimum lengths of unvisited vertices
+			int min = 100000;
+			for (int i = 0;i< vertices.size();i++){
+				if ((getIndex(unvisited, vertices[i] != -1) && (shortestLengths[i] < min)){
+					min = shortestLengths[i];
+					current = vertices[i];
+					currIndex = i;
+				}
+			}
+
+			//for current vertex, examine distance of each neighbor from starting vertex and update accordingly for shortest length
+			for (int i = 0; i < current->paths; i++){
+				vertex* neighbor = current->paths[i];
+				int neighborIndex = getIndex(current->paths,neighbor);
+				if (getIndex(unvisited, neighbor) != -1){
+					int neighborDistance = shortestLengths[currIndex] + getEdgeWeight(current, neighbor);
+					if (neighborDistance < shortestLengths[neighborIndex]){
+						shortestLengths[neighborIndex] = neighborDistance;
+						prevVertex[neighborIndex] = current;
+					}
+				}
+			}
+
+			//Move vertex from unvisited to visited
+			visited.push_back(current);
+			unvisited.erase(unvisited.begin() + getIndex(unvisited, current));
+		}
+		
+		//use v2
+		
 		
 
-		//unvisited.erase(unvisited.begin());
+
 		return 0;
 	}
 
@@ -62,7 +124,7 @@ int main(){
 	bool run = true;
 
 	while (run){
-		cout << "Action ([A]dd [V]ertex, [A]dd [E]dge, [S]how graph, [F]ind shortest path, [Q]uit):" << endl;
+		cout << "Actions:\n[A]dd [V]ertex\n[A]dd [E]dge\n[D]elete [V]ertex\n[D]elete [E]dge\n[S]how graph\n[F]ind shortest path\n[Q]uit):" << endl;
 		cin >> command;
 
 		if (command == "AV"){
@@ -70,6 +132,13 @@ int main(){
 			cout << "Enter name: ";
 			cin >> VertexName;
 			GRAPH->addVertex(VertexName); //Add confirmation that name dosen't alreay exist
+		}
+		else if (command == "DV"){
+			string VertexName;
+			cout << "Enter name: ";
+			cin >> VertexName;
+			vertex* deleteMe = GRAPH->locate(VertexName);
+			GRAPH->deleteVertex(deleteMe);
 		}
 		else if (command == "AE"){
 			string VertexName1, VertexName2;
@@ -80,7 +149,14 @@ int main(){
 			vertex* v2 = GRAPH->locate(VertexName2);
 			if (v1 && v2) {cout << "Weight: "; cin >> weight; GRAPH->addEdge(v1,v2,weight);}
 			else{cout << "Invalid vertices" << endl;}
-
+		}
+		else if (command == "DE"){
+			string VertexName1, VertexName2;
+			cout << "Enter vertices (" " in between): ";
+			cin >> VertexName1 >> VertexName2;
+			vertex* v1 = GRAPH->locate(VertexName1);
+			vertex* v2 = GRAPH->locate(VertexName2);
+			GRAPH->deleteEdge(v1,v2);
 		}
 		else if (command == "F"){
 			string VertexName1, VertexName2;
